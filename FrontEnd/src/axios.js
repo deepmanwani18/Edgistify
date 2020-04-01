e5d0ce9e58d0def3567c8c538823ca2eff29c63c
unknown <deepmanwani18@gmail.com>
2020-04-01 16:11:07 +0530

    update axios.js

--- FrontEnd/src/axios.js
+++ FrontEnd/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'http://13.235.43.83:8000',
     // headers: {
     //     'Content-Type': 'application/json',
     //     'Authorization': window.localStorage.getItem('Authorization'),
